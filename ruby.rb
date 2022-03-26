fc4e8e41c30ddf715595f2ca3a5248b8aa376d08
Ben <robinsonbena@gmail.com>
2022-03-26 16:37:38 -0700

    testing to/from YAML with a nested class

--- ruby.rb
+++ ruby.rb
@@ -1,11 +1,12 @@
 require 'yaml'
 
 class Test
-  attr_accessor :name, :age, :sign
+  attr_accessor :name, :age, :sign, :wand
   def initialize
-    @name = "harry"
+    @name = "ron"
     @age = "18"
-    @sign = "gryffindor"
+    @sign = "toad"
+    @wand = Wand.new
   end
 
   # Uses file.exist to see if directory exists, if not, creates directory
@@ -13,11 +14,13 @@ class Test
   # YAML.dump stores a hash of desired values to serialize 
   def to_yaml
     Dir.mkdir('saved_games') unless File.exist?('saved_games')
-    f = File.open('saved_games/second_yaml.yml', 'w')
+    f = File.open('saved_games/third_yaml.yml', 'w')
     YAML.dump({
       :name => @name,
       :age => @age,
-      :sign => @sign
+      :sign => @sign, 
+      :wand_length => @wand.length,
+      :wand_name => @wand.name
     }, f)
     f.close
   end
@@ -27,23 +30,36 @@ class Test
   # In this case the existing class instance variables are set to YAML values
   # you could also create a new class instance by passing values into class.new as parameters
   def from_yaml
-    f = YAML.load(File.read('./saved_games/second_yaml.yml'))
+    f = YAML.load(File.read('./saved_games/third_yaml.yml'))
     @name = f[:name]
     @age = f[:age]
     @sign = f[:sign]
+    @wand.length = f[:wand_length]
+    @wand.name = f[:wand_name]
   end
 
+end
+
+class Wand
+attr_accessor :length, :name
+
+  def initialize
+    @length = 1000
+    @name = "twiggy"
+  end
 end
 
 ben = Test.new
 puts ben.name
 puts ben.age
 puts ben.sign
-# ben.to_yaml   # uncomment to make yaml
-
-# manually change name sign and age
+puts ben.wand.length
+puts ben.wand.name
+# ben.to_yaml
 ben.from_yaml
 puts ben.name
 puts ben.age
 puts ben.sign
-# name sign and age should be back to original
+puts ben.wand.length
+puts ben.wand.name
+
